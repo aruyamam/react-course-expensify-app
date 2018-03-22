4f19579bde735791199d217371e97c3a3d3f6ecb
aruyamam <aruyamam@gmail.com>
2018-03-22 10:04:53 +0900

    Section9 Ract-Router 101

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './compenents/IndecisionApp';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPagee = () => (
+  <div>
+    This is from my dashboard compenent
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from my expense compenent
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+  This is from my edit compenent
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+  This is from my help compenent
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPagee} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));

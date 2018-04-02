5f9be62cf26694b86bdffd4842bd24ad50f81870
aruyamam <aruyamam@gmail.com>
2018-04-02 15:35:28 +0900

    Section9 Organizing Our Routes

--- src/app.js
+++ src/app.js
@@ -1,64 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AppRouter from './routers/AppRouter'
 import 'normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPagee = () => (
-  <div>
-    This is from my dashboard compenent
-  </div>
-);
-
-const AddExpensePage = () => (
-  <div>
-    This is from my expense compenent
-  </div>
-);
-
-const EditExpensePage = () => (
-  <div>
-    This is from my edit compenent
-  </div>
-);
-
-const HelpPage = () => (
-  <div>
-    This is from my help compenent
-  </div>
-);
-
-const NotFoundPage = () => (
-  <div>
-    404! - <Link to="/">Go home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <ul>
-      <li><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></li>
-      <li><NavLink to="/create" activeClassName="is-active">Create Expense</NavLink></li>
-      <li><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></li>
-      <li><NavLink to="/help" activeClassName="is-active">Help</NavLink></li>
-    </ul>
-  </header>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPagee} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+  <div>
+    This is from my expense compenent
+  </div>
+);
+
+export default AddExpensePage;
